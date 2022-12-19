44595989eb599e94096e3c3011e6127865e61bc8
phil-mel <120044497+phil-mel@users.noreply.github.com>
2022-12-19 16:37:18 +0000

    Create remove-orphaned-users.ps1

---
+++ remove-orphaned-users.ps1
@@ -0,0 +1,27 @@
+#script designed to pull all users with both read and send as permissions
+#then identify orphaned users by their SID strings
+#and remove permissions from all users with SID strings
+
+Connect-ExchangeOnline -UserPrincipalName [<credentials>]
+
+$readperm = (get-mailboxpermission -identity [<MB>]).user
+$readarray = (0..(($readperm.count)-1))
+foreach ($number in $readarray)
+{
+    if ($readperm[$number] -like "*S-1-5-21-*")
+        {
+            remove-mailboxpermission -identity [<MB>] -user $readperm[$number] -accessrights fullaccess
+        }
+        
+}
+
+$sendperm = (get-recipientpermission -identity [<MB>]).user
+$sendarray = (0..(($sendperm.count)-1))
+foreach ($number in $sendarray)
+{
+    if ($sendperm[$number] -like "*S-1-5-21-*")
+        {
+            remove-recipientpermission [<MB>] -AccessRights SendAs -Trustee $sendperm[$number]
+        }
+        
+}
